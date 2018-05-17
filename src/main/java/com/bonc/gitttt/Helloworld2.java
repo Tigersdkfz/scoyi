65aadde55f56d7fa69d25c17034015f37de69369
ciri <634220314@qq.com>
2018-05-17 15:35:11 +0800

    这是一个helloworld
    Signed-off-by: ciri <634220314@qq.com>

---
+++ src/main/java/com/bonc/gitttt/Helloworld2.java
@@ -0,0 +1,8 @@
+package com.bonc.gitttt;
+
+public class Helloworld2 {
+    public static void main(String[] args) {
+        System.out.println("this is the second helloworld!!!!!!!!!!!!!");
+        System.out.println("adskjlbhkl");
+    }
+}
